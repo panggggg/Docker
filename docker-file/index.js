d88d7ff72991b29e6dc4fbeb45095491e944e596
panggggg <p.picczy@gmail.com>
2021-06-16 09:02:34 +0700

    no message

--- docker-file/index.js
+++ docker-file/index.js
@@ -1,10 +1,18 @@
 const express = require("express")
+const redis = require("redis")
+
 const app = express()
+const redisClient = redis.createClient();
+redisClient.set('visits', 0);
 
-app.get("/", (req, res) => {
-    res.send("Hello");
+app.get('/', (req, res) => {
+    redisClient.get('visits', (err, visits) => {
+        res.send('Number of visits is ' + visits);
+        redisClient.set('visits', parseInt(visits) + 1);
+    });
+});
+
+app.listen(8081, () => {
+    console.log('Listening on port 8081');
 })
 
-app.listen(3000, () => {
-    console.log("Start app port: 3000")
-})
